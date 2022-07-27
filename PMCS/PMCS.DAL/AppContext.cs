e444123ac401c7577a2183246c5958cb3f2445e6
Daniil Feoktistov <whatdoessence@gmail.com>
2022-07-27 16:16:15 +0300

    add db context

---
+++ PMCS/PMCS.DAL/AppContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMCS.DAL.Entities;
+
+namespace PMCS.DAL
+{
+    public sealed class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<MealEntity> Meals { get; set; }
+        public DbSet<WalkingEntity> Walkings { get; set; }
+        public DbSet<OwnerEntity> Owners { get; set; }
+        public DbSet<PetEntity> Pets { get; set; }
+        public DbSet<VaccineEntity> Vaccines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
