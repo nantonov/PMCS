6a5710397f92120772dbb9abf62324e524a39cf0
Daniil Feoktistov <whatdoessence@gmail.com>
2022-07-29 13:49:17 +0300

    Prefer Int ID's to Guid

--- PMCS/PMCS.DAL/Entities/VaccineEntity.cs
+++ PMCS/PMCS.DAL/Entities/VaccineEntity.cs
@@ -4,12 +4,12 @@ namespace PMCS.DAL.Entities
 {
     public class VaccineEntity : IHasIdEntity
     {
-        public Guid Id { get; init; }
+        public int Id { get; init; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime DateTime { get; set; }
 
-        public Guid PetId { get; init; }
+        public int PetId { get; init; }
         public PetEntity Pet { get; init; }
     }
 }
--- PMCS/PMCS.DAL/Entities/WalkingEntity.cs
+++ PMCS/PMCS.DAL/Entities/WalkingEntity.cs
@@ -4,13 +4,13 @@ namespace PMCS.DAL.Entities
 {
     public class WalkingEntity : IHasIdEntity
     {
-        public Guid Id { get; init; }
+        public int Id { get; init; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime Stared { get; set; }
         public DateTime Finished { get; set; }
 
-        public Guid PetId { get; init; }
+        public int PetId { get; init; }
         public PetEntity Pet { get; init; }
     }
 }
