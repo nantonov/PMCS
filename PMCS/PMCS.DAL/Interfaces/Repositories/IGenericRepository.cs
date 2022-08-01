2f801d841f8f8cd7d2117920a11723fc5d8ec9ce
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-01 11:14:44 +0300

    Implement GenericRepository

---
+++ PMCS/PMCS.DAL/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PMCS.DAL.Interfaces.Entities;
+
+namespace PMCS.DAL.Interfaces.Repositories
+{
+    interface IGenericRepository <TEntity> where TEntity : IHasIdEntity
+    {
+        Task<TEntity> Insert(TEntity entity, CancellationToken cancellationToken);
+        Task<IEnumerable<TEntity>> Get(CancellationToken cancellationToken);
+        Task<TEntity> GetById(int id, CancellationToken cancellationToken);
+        Task<TEntity> Update(TEntity entity, CancellationToken cancellationToken);
+        Task<TEntity> Delete(int id, CancellationToken cancellationToken);
+    }
+}
