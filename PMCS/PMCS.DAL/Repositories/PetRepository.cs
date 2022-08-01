9622e103003d0968898aa9110ca9c651628c218b
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-01 11:53:57 +0300

    Implement PetRepository

---
+++ PMCS/PMCS.DAL/Repositories/PetRepository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMCS.DAL.Entities;
+using PMCS.DAL.Interfaces.Repositories;
+
+namespace PMCS.DAL.Repositories
+{
+    public class PetRepository : GenericRepository<PetEntity>, IPetRepository
+    {
+        public PetRepository(AppContext context) : base(context) { }
+        public override async Task<IEnumerable<PetEntity>> Get(CancellationToken cancellationToken)
+        {
+            return await _dbSet.Include(x => x.Vaccines).
+                                Include(x=>x.Walkings).
+                                Include(x=>x.Meals).
+                                Include(x=>x.Owner).
+                                ToListAsync(cancellationToken);
+        }
+
+        public override async Task<PetEntity> GetById(int id, CancellationToken cancellationToken)
+        {
+            return await Query.Include(x => x.Vaccines).
+                               Include(x => x.Walkings).
+                               Include(x => x.Meals).
+                               Include(x => x.Owner).
+                               FirstOrDefaultAsync(x=>x.Id==id, cancellationToken);
+        }
+    }
+}
