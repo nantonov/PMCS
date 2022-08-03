f6f7a86b6916ba5aa579623164b25a6d0a49c215
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-03 14:08:05 +0300

    Implement Owner and Pet validators (#11)

---
+++ PMCS/PMCS.API/Constants/PetValidationParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace PMCS.API.Constants
+{
+    public static class PetValidationParameters
+    {
+        public const int MaxDurationOfLife = 70;
+        public const int MaxNameLength = 3;
+        public const int MinNameLength = 30;
+        public const int MaxInfoLength = 5;
+        public const int MinInfoLength = 300;
+        public const float MinWeight = 0.05f;
+        public const float MaxWeight = 1000.0f;
+
+        public const string NameRegularExpression = "^[a-zA-Z0-9 ]*$";
+    }
+}
