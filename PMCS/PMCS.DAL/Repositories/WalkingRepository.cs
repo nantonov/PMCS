283035c98597dff1ba352593cc7ce3b202423c52
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 00:27:11 +0300

    update walking repository: add missed eager loading

--- PMCS/PMCS.DAL/Repositories/WalkingRepository.cs
+++ PMCS/PMCS.DAL/Repositories/WalkingRepository.cs
@@ -1,10 +1,20 @@
-﻿using PMCS.DAL.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using PMCS.DAL.Entities;
 using PMCS.DAL.Interfaces.Repositories;
 
 namespace PMCS.DAL.Repositories
 {
     public class WalkingRepository : GenericRepository<WalkingEntity>, IWalkingRepository
     {
-        public WalkingRepository (AppContext context) : base(context) { }
+        public WalkingRepository(AppContext context) : base(context) { }
+        public override async Task<IEnumerable<WalkingEntity>> Get(CancellationToken cancellationToken)
+        {
+            return await _dbSet.Include(x => x.Pet).ToListAsync(cancellationToken);
+        }
+
+        public override async Task<WalkingEntity> GetById(int id, CancellationToken cancellationToken)
+        {
+            return await Query.Include(x => x.Pet).FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
     }
 }
