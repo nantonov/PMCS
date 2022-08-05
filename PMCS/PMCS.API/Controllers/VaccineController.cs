33e44a5e71413a9bced9c2d141ac103b82641fac
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 12:19:28 +0300

    Implement controller

---
+++ PMCS/PMCS.API/Controllers/VaccineController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using PMCS.API.Validators;
+using PMCS.API.ViewModels.Vaccine;
+using PMCS.DLL.Interfaces.Services;
+using PMCS.DLL.Models;
+
+namespace PMCS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VaccineController : ControllerBase
+    {
+        private readonly IVaccineService _service;
+        private readonly IMapper _mapper;
+        private readonly PostVaccineValidator _postVaccineValidator;
+        private readonly UpdateVaccineValidator _updateVaccineValidator;
+
+        public VaccineController(IVaccineService service, IMapper mapper, PostVaccineValidator postVaccineValidator, UpdateVaccineValidator updateVaccineValidator)
+        {
+            _service = service;
+            _mapper = mapper;
+            _postVaccineValidator = postVaccineValidator;
+            _updateVaccineValidator = updateVaccineValidator;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<VaccineViewModel>> GetAll(CancellationToken cancellationToken)
+        {
+            return _mapper.Map<IEnumerable<VaccineViewModel>>(await _service.GetAll(cancellationToken));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<VaccineViewModel> GetById(int id, CancellationToken cancellationToken)
+        {
+            return _mapper.Map<VaccineViewModel>(await _service.GetById(id, cancellationToken));
+        }
+
+        [HttpPost]
+        public async Task<VaccineViewModel> Add([FromBody] PostVaccineViewModel viewModel, CancellationToken cancellationToken)
+        {
+            await _postVaccineValidator.ValidateAndThrowAsync(viewModel, cancellationToken);
+
+            var model = _mapper.Map<VaccineModel>(viewModel);
+
+            return _mapper.Map<VaccineViewModel>(await _service.Add(model, cancellationToken));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id, CancellationToken cancellationToken)
+        {
+            await _service.Delete(id, cancellationToken);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<VaccineViewModel> Update(int id, [FromBody] UpdateVaccineViewModel viewModel, CancellationToken cancellationToken)
+        {
+            await _updateVaccineValidator.ValidateAndThrowAsync(viewModel, cancellationToken);
+
+            var model = _mapper.Map<VaccineModel>(viewModel);
+
+            model.Id = id;
+
+            return _mapper.Map<VaccineViewModel>(await _service.Update(model, cancellationToken));
+        }
+    }
+}
