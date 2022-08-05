e8f36001c5845c7ee1bd03332e85c0b080230e62
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 12:03:38 +0300

    Implement the rest view models

--- PMCS/PMCS.API/ViewModels/Vaccine/VaccineShortViewModel.cs
+++ PMCS/PMCS.API/ViewModels/Vaccine/VaccineShortViewModel.cs
@@ -1,11 +1,10 @@
 ﻿namespace PMCS.API.ViewModels.Vaccine
 {
-    public class PostVaccineViewModel
+    public class VaccineShortViewModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string? Description { get; set; }
         public DateTime DateTime { get; set; }
-
-        public int PetId { get; set; }
     }
 }
