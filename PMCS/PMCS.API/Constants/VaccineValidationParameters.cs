13d998b28b76f21e9d303d6d35ba26e556797d1d
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-05 12:21:48 +0300

    Implement vaccine CRUD (#17)

---
+++ PMCS/PMCS.API/Constants/VaccineValidationParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace PMCS.API.Constants
+{
+    public static class VaccineValidationParameters
+    {
+        public const int MinTitleLength = 3;
+        public const int MaxTitleLength = 50;
+        public const int MinDescriptionLength = 1;
+        public const int MaxDescriptionLength = 100;
+    }
+}
---
+++ PMCS/PMCS.API/Validators/PostVaccineValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using PMCS.API.Resources.Validators;
+using PMCS.API.ViewModels.Vaccine;
+using static PMCS.API.Constants.VaccineValidationParameters;
+using static PMCS.API.Helpers.VaccineValidatorHelper;
+
+namespace PMCS.API.Validators
+{
+    public class PostVaccineValidator : AbstractValidator<PostVaccineViewModel>
+    {
+        public PostVaccineValidator()
+        {
+            RuleFor(x => x.PetId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage(PostVaccineValidatorResources.IdShouldBeGreaterThanZero);
+            RuleFor(x => x.Description)
+                .Length(MinDescriptionLength, MaxDescriptionLength)
+                .WithMessage(PostVaccineValidatorResources.IncorrectDescriptionLength);
+            RuleFor(x => x.DateTime)
+                .NotEmpty()
+                .Must(IsAValidDate)
+                .WithMessage(PostVaccineValidatorResources.IncorrectDateTime);
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .Length(MinTitleLength, MaxTitleLength)
+                .WithMessage(PostVaccineValidatorResources.IncorrectTitleLength);
+        }
+    }
+}
