de94cc77644e1993998ef37cf260b94fcfc23b10
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 12:28:09 +0300

    Implement fluent validation

---
+++ PMCS/PMCS.API/Constants/WalkingValidationParameters.cs
@@ -0,0 +1,12 @@
+﻿namespace PMCS.API.Constants
+{
+    public static class WalkingValidationParameters
+    {
+        public const int MinTitleLength = 3;
+        public const int MaxTitleLength = 50;
+        public const int MinDescriptionLength = 1;
+        public const int MaxDescriptionLength = 150;
+        public static int MaxWalkingDurationInSeconds = 28800;
+        public static int MinWalkingDurationInSeconds = 60;
+    }
+}
---
+++ PMCS/PMCS.API/Helpers/WalkingValidatorHelper.cs
@@ -0,0 +1,19 @@
+﻿using static PMCS.API.Constants.WalkingValidationParameters;
+
+namespace PMCS.API.Helpers
+{
+    public class WalkingValidatorHelper
+    {
+        public static bool IsAValidDate(DateTime date)
+        {
+            return date != null && date <= DateTime.UtcNow;
+        }
+
+        public static bool IsWalkingDurationIsValid(DateTime started, DateTime finished)
+        {
+            TimeSpan walkingDuration = finished - started;
+
+            return walkingDuration.TotalSeconds > MinWalkingDurationInSeconds && walkingDuration.TotalSeconds < MaxWalkingDurationInSeconds;
+        }
+    }
+}
