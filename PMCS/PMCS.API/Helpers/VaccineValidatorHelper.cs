3867adced52d61a6da16e8108386d637a578ae5e
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 12:19:21 +0300

    Add fluent validation

---
+++ PMCS/PMCS.API/Helpers/VaccineValidatorHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace PMCS.API.Helpers
+{
+    public class VaccineValidatorHelper
+    {
+        public static bool IsAValidDate(DateTime date)
+        {
+            return date != null && date <= DateTime.UtcNow;
+        }
+    }
+}
---
+++ PMCS/PMCS.API/Validators/UpdateVaccineValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using PMCS.API.Resources.Validators;
+using PMCS.API.ViewModels.Vaccine;
+using static PMCS.API.Constants.VaccineValidationParameters;
+using static PMCS.API.Helpers.VaccineValidatorHelper;
+
+namespace PMCS.API.Validators
+{
+    public class UpdateVaccineValidator : AbstractValidator<UpdateVaccineViewModel>
+    {
+        public UpdateVaccineValidator()
+        {
+            RuleFor(x => x.PetId)
+                .NotEmpty()
+                .GreaterThan(0)
+                .WithMessage(UpdateVaccineValidatorResources.IdShouldBeGreaterThanZero);
+            RuleFor(x => x.Description)
+                .Length(MinDescriptionLength, MaxDescriptionLength)
+                .WithMessage(UpdateVaccineValidatorResources.IncorrectDescriptionLength);
+            RuleFor(x => x.DateTime)
+                .NotEmpty()
+                .Must(IsAValidDate)
+                .WithMessage(UpdateVaccineValidatorResources.IncorrectDateTime);
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .Length(MinTitleLength, MaxTitleLength)
+                .WithMessage(UpdateVaccineValidatorResources.IncorrectTitleLength);
+        }
+    }
+}
