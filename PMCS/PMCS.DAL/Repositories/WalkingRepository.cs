e17ee2eddf019d988eb4f1db5e0201a9933c6c13
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-05 12:27:38 +0300

    Add WalkingRepository

---
+++ PMCS/PMCS.DAL/Repositories/WalkingRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMCS.DAL.Entities;
+using PMCS.DAL.Interfaces.Repositories;
+
+namespace PMCS.DAL.Repositories
+{
+    public class WalkingRepository : GenericRepository<WalkingEntity>, IWalkingRepository
+    {
+        public WalkingRepository(AppContext context) : base(context) { }
+        public override async Task<IEnumerable<WalkingEntity>> Get(CancellationToken cancellationToken)
+        {
+            return await _dbSet.Include(x => x.Pet).ToListAsync(cancellationToken);
+        }
+
+        public override async Task<WalkingEntity> GetById(int id, CancellationToken cancellationToken)
+        {
+            return await Query.Include(x => x.Pet).FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+        }
+    }
+}
