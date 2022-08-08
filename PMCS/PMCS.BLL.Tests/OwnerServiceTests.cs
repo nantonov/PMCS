98ff3d8363f203e1e3f245d5149ca63587f11465
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-08 14:57:41 +0300

    Implement unit tests for OwnerService (#19)

---
+++ PMCS/PMCS.BLL.Tests/OwnerServiceTests.cs
@@ -0,0 +1,150 @@
+using AutoMapper;
+using Moq;
+using PMCS.DAL.Entities;
+using PMCS.DAL.Interfaces.Repositories;
+using PMCS.DLL.Exceptions;
+using PMCS.DLL.Models;
+using PMCS.DLL.Services;
+using static PMCS.BLL.Tests.Entities.TestOwnerEntity;
+using static PMCS.BLL.Tests.Models.TestOwnerModel;
+
+namespace PMCS.BLL.Tests
+{
+    public class OwnerServiceTests
+    {
+        private readonly Mock<IOwnerRepository> _ownerRepositoryMock = new();
+
+        private readonly Mock<IMapper> _mapperMock = new();
+
+        [Fact]
+        public async Task GetById_ShouldReturnOwner_WhenOwnerExists()
+        {
+            var expectedOwner = ValidOwnerEntity;
+
+            _ownerRepositoryMock.Setup(x => x.GetById(expectedOwner.Id, default)).ReturnsAsync(expectedOwner);
+            _mapperMock.Setup(m => m.Map<OwnerModel>(expectedOwner)).Returns(ValidOwnerModel);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+
+            var actualOwner = await service.GetById(expectedOwner.Id, default);
+
+            Assert.Equal(expectedOwner.Id, actualOwner.Id);
+            Assert.Equal(expectedOwner.FullName, actualOwner.FullName);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldThrowException_WhenOwnerDoesNotExist()
+        {
+            _ownerRepositoryMock.Setup(x => x.GetById(It.IsAny<int>(), default)).ReturnsAsync(() => null);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+            async Task Act() => await service.GetById(It.IsAny<int>(), default);
+
+            await Assert.ThrowsAsync<ModelIsNotFoundException>(Act);
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnAllOwners_WhenOwnersExist()
+        {
+            var expectedOwners = ValidOwnerEntityList;
+
+            _ownerRepositoryMock.Setup(x => x.Get(default)).ReturnsAsync(ValidOwnerEntityList);
+            _mapperMock.Setup(m => m.Map<IEnumerable<OwnerModel>>(expectedOwners)).Returns(ValidOwnerModelList);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+            var actualOwners = await service.GetAll(default);
+
+            Assert.NotEmpty(actualOwners);
+            Assert.Equal(expectedOwners.Count(), actualOwners.Count());
+        }
+
+        [Fact]
+        public async Task Get_ShouldReturnEmptyList_WhenOwnersDoNotExist()
+        {
+            var expectedOwners = EmptyOwnerEntityList;
+
+            _ownerRepositoryMock.Setup(x => x.Get(default)).ReturnsAsync(EmptyOwnerEntityList);
+            _mapperMock.Setup(m => m.Map<IEnumerable<OwnerModel>>(expectedOwners)).Returns(EmptyOwnerModelList);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+            var actualOwners = await service.GetAll(default);
+
+            Assert.Empty(actualOwners);
+        }
+
+        [Fact]
+        public async Task Add_ShouldAddOwner_WhenOwnerEntityIsValid()
+        {
+            var expectedOwner = ValidOwnerModel;
+
+            _ownerRepositoryMock.Setup(x => x.GetById(ValidOwnerEntity.Id, default)).ReturnsAsync(ValidOwnerEntity);
+            _ownerRepositoryMock.Setup(x => x.Insert(ValidOwnerEntity, default)).ReturnsAsync(ValidOwnerEntity);
+            _mapperMock.Setup(m => m.Map<OwnerModel>(ValidOwnerEntity)).Returns(ValidOwnerModel);
+            _mapperMock.Setup(m => m.Map<OwnerEntity>(ValidOwnerModel)).Returns(ValidOwnerEntity);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+
+            var actualOwner = await service.Add(expectedOwner, default);
+
+            _ownerRepositoryMock.Verify(x => x.Insert(ValidOwnerEntity, default));
+            Assert.Equal(expectedOwner.Id, actualOwner.Id);
+            Assert.Equal(expectedOwner.FullName, actualOwner.FullName);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldThrowException_WhenOwnerDoesNotExist()
+        {
+            _ownerRepositoryMock.Setup(x => x.GetById(It.IsAny<int>(), default)).ReturnsAsync(() => null);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+            async Task Act() => await service.Delete(It.IsAny<int>(), default);
+
+            await Assert.ThrowsAsync<ModelIsNotFoundException>(Act);
+        }
+
+        [Fact]
+        public async Task Delete_ShouldDeleteOwner_WhenOwnerExists()
+        {
+            var owner = ValidOwnerEntity;
+
+            _ownerRepositoryMock.Setup(x => x.GetById(owner.Id, default)).ReturnsAsync(owner);
+            _ownerRepositoryMock.Setup(x => x.Delete(owner.Id, default));
+            _mapperMock.Setup(m => m.Map<OwnerModel>(ValidOwnerEntity)).Returns(ValidOwnerModel);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+            await service.Delete(owner.Id, default);
+
+            _ownerRepositoryMock.Verify(x => x.Delete(owner.Id, default));
+        }
+
+        [Fact]
+        public async Task Update_ShouldUpdateOwner_WhenOwnerExists()
+        {
+            var expectedOwner = ValidOwnerModel;
+
+            _ownerRepositoryMock.Setup(x => x.GetById(ValidOwnerEntity.Id, default)).ReturnsAsync(ValidOwnerEntity);
+            _ownerRepositoryMock.Setup(x => x.Update(ValidOwnerEntity, default)).ReturnsAsync(ValidOwnerEntity);
+            _mapperMock.Setup(m => m.Map<OwnerModel>(ValidOwnerEntity)).Returns(ValidOwnerModel);
+            _mapperMock.Setup(m => m.Map<OwnerEntity>(ValidOwnerModel)).Returns(ValidOwnerEntity);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+
+            var actualOwner = await service.Update(expectedOwner, default);
+
+            _ownerRepositoryMock.Verify(x => x.Update(ValidOwnerEntity, default));
+            Assert.Equal(expectedOwner.FullName, actualOwner.FullName);
+        }
+
+        [Fact]
+        public async Task Update_ShouldThrowException_WhenOwnerDoesNotExist()
+        {
+            _ownerRepositoryMock.Setup(x => x.GetById(It.IsAny<int>(), default)).ReturnsAsync(() => null);
+
+            var service = new OwnerService(_ownerRepositoryMock.Object, _mapperMock.Object);
+
+            async Task Act() => await service.Update(ValidOwnerModel, default);
+
+            await Assert.ThrowsAsync<ModelIsNotFoundException>(Act);
+        }
+    }
+}
