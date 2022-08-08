5d92a5223f004af2deb51f294eaed4fa25d6e427
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-08 17:55:54 +0300

    Implement VaccineService unit tests

---
+++ PMCS/PMCS.BLL.Tests/Entities/TestVaccineEntity.cs
@@ -0,0 +1,32 @@
+﻿using PMCS.DAL.Entities;
+
+namespace PMCS.BLL.Tests.Entities
+{
+    public class TestVaccineEntity
+    {
+        public static VaccineEntity ValidVaccineEntity = new VaccineEntity()
+        {
+            Id = 1,
+            PetId = 1,
+            Title = "Test"
+        };
+
+        public static IEnumerable<VaccineEntity> ValidVaccineEntityList = new List<VaccineEntity>()
+        {
+            new VaccineEntity()
+            {
+                Id = 1,
+                PetId = 1,
+                Title = "Test1"
+            },
+            new VaccineEntity()
+            {
+                Id = 2,
+                PetId = 1,
+                Title = "Test2"
+            }
+        };
+
+        public static IEnumerable<VaccineEntity> EmptyVaccineEntityList = new List<VaccineEntity>() { };
+    }
+}
