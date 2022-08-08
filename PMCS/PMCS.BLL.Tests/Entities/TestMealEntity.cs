342e697c5aaecac261df311e60ec511c918902c4
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-08 16:56:00 +0300

    Add basic models and entities for test

---
+++ PMCS/PMCS.BLL.Tests/Entities/TestMealEntity.cs
@@ -0,0 +1,33 @@
+﻿using PMCS.DAL.Entities;
+
+namespace PMCS.BLL.Tests.Entities
+{
+    public static class TestMealEntity
+    {
+        public static MealEntity ValidOwnerEntity = new MealEntity()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+            DateTime = DateTime.Today
+        };
+
+        public static IEnumerable<MealEntity> ValidOwnerEntityList = new List<MealEntity>()
+        {
+            new MealEntity()
+            {
+                Id = 1,
+                Title = "Test1",
+                PetId = 1,
+                DateTime = DateTime.Today
+            },
+            new MealEntity()
+            {
+                Id = 2,
+                Title = "Test2",
+                PetId = 1,
+                DateTime = DateTime.Today
+            }
+        };
+    }
+}
---
+++ PMCS/PMCS.BLL.Tests/Models/TestMealModel.cs
@@ -0,0 +1,35 @@
+﻿using PMCS.DLL.Models;
+
+namespace PMCS.BLL.Tests.Models
+{
+    public static class TestMealModel
+    {
+        public static MealModel ValidOwnerEntity = new MealModel()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+            DateTime = DateTime.Today
+        };
+
+        public static IEnumerable<MealModel> ValidOwnerEntityList = new List<MealModel>()
+        {
+            new MealModel()
+            {
+                Id = 1,
+                Title = "Test1",
+                PetId = 1,
+                DateTime = DateTime.Today
+            },
+            new MealModel()
+            {
+                Id = 2,
+                Title = "Test2",
+                PetId = 1,
+                DateTime = DateTime.Today
+            }
+        };
+
+        public static IEnumerable<MealModel> EmptyOwnerEntityList = new List<MealModel>() { };
+    }
+}
