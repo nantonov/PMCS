7492641bd8c82a8658ba17f2e4eb60fb490d86a5
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-08 17:34:13 +0300

    Implement MealService unit tests (#21)

---
+++ PMCS/PMCS.BLL.Tests/Entities/TestMealEntity.cs
@@ -0,0 +1,35 @@
+﻿using PMCS.DAL.Entities;
+
+namespace PMCS.BLL.Tests.Entities
+{
+    public static class TestMealEntity
+    {
+        public static MealEntity ValidMealEntity = new MealEntity()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+            DateTime = DateTime.Today
+        };
+
+        public static IEnumerable<MealEntity> ValidMealEntityList = new List<MealEntity>()
+        {
+            new MealEntity()
+            {
+                Id = 1,
+                Title = "Test1",
+                PetId = 1,
+                DateTime = DateTime.Today
+            },
+            new MealEntity()
+            {
+                Id = 2,
+                Title = "Test2",
+                PetId = 1,
+                DateTime = DateTime.Today
+            }
+        };
+
+        public static IEnumerable<MealEntity> EmptyMealEntityList = new List<MealEntity>() { };
+    }
+}
