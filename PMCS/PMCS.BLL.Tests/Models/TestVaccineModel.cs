eec930c6bbaba0ccae5180eea897701dc6fbde3b
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-09 16:18:57 +0300

    Implement VaccineService unit tests (#22)

---
+++ PMCS/PMCS.BLL.Tests/Models/TestVaccineModel.cs
@@ -0,0 +1,32 @@
+﻿using PMCS.DLL.Models;
+
+namespace PMCS.BLL.Tests.Models
+{
+    public static class TestVaccineModel
+    {
+        public static VaccineModel ValidVaccineModel = new VaccineModel()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+        };
+
+        public static IEnumerable<VaccineModel> ValidVaccineModelList = new List<VaccineModel>()
+        {
+            new VaccineModel()
+            {
+                Id = 1,
+                Title = "Test1",
+                PetId = 1,
+            },
+            new VaccineModel()
+            {
+                Id = 2,
+                Title = "Test2",
+                PetId = 1,
+            }
+        };
+
+        public static IEnumerable<VaccineModel> EmptyVaccineModelList = new List<VaccineModel>() { };
+    }
+}
