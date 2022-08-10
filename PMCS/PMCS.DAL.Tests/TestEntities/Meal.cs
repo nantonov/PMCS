29eeb41ef655e37276fa36ef43c142d1ebadb8a9
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-10 15:55:56 +0300

    Implement MealRepository tests (#26)

---
+++ PMCS/PMCS.DAL.Tests/TestEntities/Meal.cs
@@ -0,0 +1,53 @@
+﻿using PMCS.DAL.Entities;
+using static PMCS.DAL.Tests.TestEntities.Pet;
+
+namespace PMCS.DAL.Tests.TestEntities
+{
+    public static class Meal
+    {
+        public static MealEntity ValidMealEntity = new MealEntity()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+            Pet = PetEntityForMealTest
+        };
+
+        public static MealEntity MealEntityWithInexistentId = new MealEntity()
+        {
+            Id = 1000,
+            Title = "Test",
+            PetId = 1
+        };
+
+        public static MealEntity MealEntityToInsert = new MealEntity()
+        {
+            Id = 4,
+            Title = "Inserted",
+            PetId = 1,
+            Pet = PetEntityForMealTest
+        };
+
+        public static MealEntity MealEntityToUpdate = new MealEntity() { Id = 10, Title = "Old Title", PetId = 1, Pet = PetEntityForMealTest };
+
+        public static MealEntity UpdatedMealEntity = new MealEntity() { Id = 10, Title = "New Title", PetId = 1, Pet = PetEntityForMealTest };
+
+        public static IEnumerable<MealEntity> ValidMealEntityList = new List<MealEntity>()
+        {
+            new MealEntity()
+            {
+                Id = 1,
+                Title = "First Entity",
+                PetId = 1,
+                Pet = ValidPetEntity
+            },
+            new MealEntity()
+            {
+                Id = 2,
+                Title = "Second Entity",
+                PetId = 1,
+                Pet = ValidPetEntity
+            },
+        };
+    }
+}
