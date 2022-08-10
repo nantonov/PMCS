908280334a81df6db1bd7727fd6d84ac979f6635
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-10 17:45:22 +0300

    Add basic test pet entities

---
+++ PMCS/PMCS.API.Tests/Entities/PetEntities.cs
@@ -0,0 +1,49 @@
+﻿using PMCS.DAL.Entities;
+using static PMCS.API.Tests.Entities.OwnerEntities;
+
+namespace PMCS.API.Tests.Entities
+{
+    public static class PetEntities
+    {
+        public static PetEntity ValidPetEntity = new PetEntity()
+        {
+            Id = 1,
+            Name = "Test Owner",
+            OwnerId = 1,
+            Owner = ValidOwnerEntityForPetTests
+        };
+
+        public static PetEntity PetEntityWithInvalidId = new PetEntity()
+        {
+            Id = 999,
+            Name = "Test Owner",
+            OwnerId = 1,
+            Owner = ValidOwnerEntityForPetTests
+        };
+
+        public static IEnumerable<PetEntity> ValidPetEntityList = new List<PetEntity>()
+        {
+            new PetEntity()
+            {
+                Id = 1,
+                Name = "First Owner",
+                OwnerId = 1,
+                Owner = ValidOwnerEntityForPetTests
+            },
+            new PetEntity()
+            {
+                Id = 2,
+                Name = "Second Owner",
+                OwnerId = 1,
+                Owner = ValidOwnerEntityForPetTests
+            },
+            new PetEntity()
+            {
+                Id = 3,
+                Name = "Third Owner",
+                OwnerId = 1,
+                Owner = ValidOwnerEntityForPetTests
+            }
+        };
+    }
+}
