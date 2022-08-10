25db10add2d5b8e8337e354c62cc736f0ddc0483
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-10 17:45:48 +0300

    Implement PetControllerTests

---
+++ PMCS/PMCS.API.Tests/PetControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using static PMCS.API.Tests.Entities.PetEntities;
+
+namespace PMCS.API.Tests
+{
+    public class PetControllerTests : APIIntegrationTestsBase
+    {
+        [Fact]
+        public async Task GetAll_PetsExist_ReturnsOwnersListWithOKStatusCode()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            await _context.Pets.AddRangeAsync(ValidPetEntityList);
+            await _context.SaveChangesAsync();
+
+            var response = await _httpClient.GetAsync("/api/Pet");
+
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotEmpty(await response.Content.ReadAsAsync<List<OwnerViewModel>>());
+        }
+    }
+}
