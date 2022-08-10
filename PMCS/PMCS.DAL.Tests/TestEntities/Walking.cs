ed5ece9ee2a86c253cbdf3554989c7fcbd14e972
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-10 12:19:39 +0300

    Implement WalkingRepository integration tests

---
+++ PMCS/PMCS.DAL.Tests/TestEntities/Walking.cs
@@ -0,0 +1,53 @@
+﻿using PMCS.DAL.Entities;
+using static PMCS.DAL.Tests.TestEntities.Pet;
+
+namespace PMCS.DAL.Tests.TestEntities
+{
+    public static class Walking
+    {
+        public static WalkingEntity ValidWalkingEntity = new WalkingEntity()
+        {
+            Id = 1,
+            Title = "Test",
+            PetId = 1,
+            Pet = PetEntityForRelatedEntitiesTests
+        };
+
+        public static WalkingEntity WalkingEntityWithInexistentId = new WalkingEntity()
+        {
+            Id = 1000,
+            Title = "Test",
+            PetId = 1
+        };
+
+        public static WalkingEntity WalkingEntityToInsert = new WalkingEntity()
+        {
+            Id = 4,
+            Title = "Inserted",
+            PetId = 1,
+            Pet = PetEntityForRelatedEntitiesTests
+        };
+
+        public static WalkingEntity WalkingEntityToUpdate = new WalkingEntity() { Id = 10, Title = "Old Title", PetId = 1, Pet = PetEntityForRelatedEntitiesTests };
+
+        public static WalkingEntity UpdatedWalkingEntity = new WalkingEntity() { Id = 10, Title = "New Title", PetId = 1, Pet = PetEntityForRelatedEntitiesTests };
+
+        public static IEnumerable<WalkingEntity> ValidWalkingEntityList = new List<WalkingEntity>()
+        {
+            new WalkingEntity()
+            {
+                Id = 1,
+                Title = "First Entity",
+                PetId = 1,
+                Pet = ValidPetEntity
+            },
+            new WalkingEntity()
+            {
+                Id = 2,
+                Title = "Second Entity",
+                PetId = 1,
+                Pet = ValidPetEntity
+            },
+        };
+    }
+}
---
+++ PMCS/PMCS.DAL.Tests/WalkingRepositoryTests.cs
@@ -0,0 +1,96 @@
+﻿using PMCS.DAL.Entities;
+using PMCS.DAL.Repositories;
+using static PMCS.DAL.Tests.TestEntities.Walking;
+
+namespace PMCS.DAL.Tests
+{
+    public class WalkingRepositoryTests : DALIntegrationTestsBase
+    {
+        private readonly IWalkingRepository _repository;
+
+        public WalkingRepositoryTests()
+        {
+            _repository = new WalkingRepository(_context);
+        }
+
+        [Fact]
+        public async Task Get_ValidId_ReturnsWalkingEntity()
+        {
+            var entity = ValidWalkingEntity;
+
+            await _repository.Insert(entity, default);
+
+            var actual = await _repository.GetById(ValidWalkingEntity.Id, default);
+
+            Assert.NotNull(actual);
+            Assert.Equal(ValidWalkingEntity.Id, actual.Id);
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+
+        [Fact]
+        public async Task Update_ValidEntity_UpdatesEntityInDataBase()
+        {
+            var entity = WalkingEntityToUpdate;
+
+            await _repository.Insert(entity, default);
+
+            entity.Title = UpdatedWalkingEntity.Title;
+            await _repository.Update(entity, default);
+
+            var actual = await _repository.GetById(entity.Id, default);
+
+            Assert.Equal(entity.Title, actual.Title);
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+
+        [Fact]
+        public async Task Get_EntitiesDoNotExist_ReturnsEmptyList()
+        {
+            var actual = await _repository.Get(default);
+
+            Assert.Empty(actual);
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+
+        [Fact]
+        public async Task Get_EntitiesExist_ReturnsWalkingEntityList()
+        {
+            var entities = new List<WalkingEntity>(ValidWalkingEntityList);
+
+            await _repository.InsertRange(entities, default);
+
+            var expected = ValidWalkingEntityList;
+            var actual = await _repository.Get(default);
+
+            Assert.NotEmpty(actual);
+            Assert.Equal(expected.Count(), actual.Count());
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+
+        [Fact]
+        public async Task Get_InexistentId_ReturnsNull()
+        {
+            var actual = await _repository.GetById(WalkingEntityWithInexistentId.Id, default);
+
+            Assert.Null(actual);
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+
+        [Fact]
+        public async Task Insert_ValidEntity_InsertsEntityIntoDataBase()
+        {
+            await _repository.Insert(WalkingEntityToInsert, default);
+
+            var actual = await _repository.GetById(WalkingEntityToInsert.Id, default);
+
+            Assert.NotNull(actual);
+
+            await _context.Database.EnsureDeletedAsync();
+        }
+    }
+}
