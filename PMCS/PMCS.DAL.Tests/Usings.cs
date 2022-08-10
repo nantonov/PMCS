0cc0a43e78917faf510417f2ef4f04d7afae0a53
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-10 12:05:55 +0300

    Remove redundant usings

--- PMCS/PMCS.DAL.Tests/Usings.cs
+++ PMCS/PMCS.DAL.Tests/Usings.cs
@@ -1,5 +1,3 @@
-global using Microsoft.AspNetCore.Mvc.Testing;
 global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.DependencyInjection;
 global using PMCS.DAL.Interfaces.Repositories;
 global using Xunit;
