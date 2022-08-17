cfb68a470d303f49c74c3ad5f10ec73966ab7cda
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-17 15:19:08 +0300

    Add swagger and automapper

--- PMCS/Services/NotificationService/NotificationService/Program.cs
+++ PMCS/Services/NotificationService/NotificationService/Program.cs
@@ -1,6 +1,28 @@
+using Notifications.BLL.DI;
+
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigurationManager configuration = builder.Configuration;
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+BusinessLogicRegistration.RegisterBusinessLogicDependencies(builder.Services);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
