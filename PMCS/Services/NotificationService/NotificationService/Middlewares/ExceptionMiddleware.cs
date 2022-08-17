d90b1fddb2cfc11f7c6ab6720ee6d2347925c946
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-17 16:14:00 +0300

    Initial

---
+++ PMCS/Services/NotificationService/NotificationService/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿namespace Notifications.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await HandleException(context, ex);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex)
+        {
+            SetResponseParameters(context);
+            LogException(context, ex);
+
+            return context.Response.WriteAsync($"{ex.Message}\nStatusCode:{context.Response.StatusCode}");
+        }
+
+        private void LogException(HttpContext? context, Exception ex)
+        {
+            _logger.LogWarning(ex, $"{ex.Message}");
+            _logger.LogWarning(ex, $"Exception in query: {context?.Request.Path}");
+        }
+
+        private void SetResponseParameters(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+        }
+    }
+}
--- PMCS/Services/NotificationService/NotificationService/Program.cs
+++ PMCS/Services/NotificationService/NotificationService/Program.cs
@@ -1,6 +1,24 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Notifications.BLL.DI;
+using Serilog;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+BusinessLogicRegistration.RegisterBusinessLogicDependencies(builder.Services);
+
+builder.Host.UseSerilog((context, loggerConfiguration) => loggerConfiguration
+    .WriteTo.Console()
+    .WriteTo.Seq("http://localhost:5341"));
+
+var app = builder.Build();
 
 app.Run();
