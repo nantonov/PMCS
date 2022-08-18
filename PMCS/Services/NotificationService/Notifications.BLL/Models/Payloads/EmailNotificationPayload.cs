5ff275aba5972ffbd2636832accbbfb2635fe3b1
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-18 11:30:02 +0300

    Add default message subject into payload

--- PMCS/Services/NotificationService/Notifications.BLL/Models/Payloads/EmailNotificationPayload.cs
+++ PMCS/Services/NotificationService/Notifications.BLL/Models/Payloads/EmailNotificationPayload.cs
@@ -1,10 +1,12 @@
-﻿namespace Notifications.BLL.Models.Payloads
+﻿using Notifications.BLL.Resources.Constants;
+
+namespace Notifications.BLL.Models.Payloads
 {
     public class EmailNotificationPayload : NotificationPayload
     {
         public string RecieverEmailAddress { get; set; }
         public string OwnerName { get; set; }
         public string PetName { get; set; }
-        public string Subject { get; set; }
+        public string Subject { get; } = EmailConfiguration.EMAIL_SUBJECT;
     }
 }
