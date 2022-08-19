e6868fbd3d55c5eec0cc9a86811136713b938d63
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-19 13:46:53 +0300

    Add exception middleware in NotificationService (#41)

---
+++ PMCS/Services/NotificationService/NotificationService/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Serilog;
+
+namespace Notifications.API.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                await HandleException(context, ex);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex)
+        {
+            SetResponseParameters(context);
+            LogException(context, ex);
+
+            return context.Response.WriteAsync($"{ex.Message}\nStatusCode:{context.Response.StatusCode}");
+        }
+
+        private void LogException(HttpContext? context, Exception ex)
+        {
+            Log.Error(ex, $"{ex.Message}");
+            Log.Error(ex, $"Exception in query: {context?.Request.Path}");
+        }
+
+        private void SetResponseParameters(HttpContext context)
+        {
+            context.Response.ContentType = "application/json";
+        }
+    }
+}
--- PMCS/Services/NotificationService/NotificationService/Program.cs
+++ PMCS/Services/NotificationService/NotificationService/Program.cs
@@ -1,11 +1,26 @@
+using Notifications.API.Middlewares;
+using Notifications.BLL.DI;
+using Serilog;
+using Serilog.Events;
 using Microsoft.AspNetCore.Http.Connections;
 using Notifications.BLL.DI;
 using Notifications.BLL.Resources.Constants;
 using Notifications.BLL.SignalR.Hubs;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigurationManager configuration = builder.Configuration;
+var configuration = new ConfigurationBuilder().Build();
+
+Log.Logger = Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("System", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .Enrich.WithThreadId()
+    .Enrich.WithThreadName()
+    .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) ({ThreadName}) {Message}{NewLine}{Exception}")
+    .CreateLogger();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -31,6 +46,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseAuthorization();
 
 app.MapControllers();
