594e9a5a11865499aad8fa7505ff3f4386edf815
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-23 15:06:42 +0300

    Implement unit tests

---
+++ PMCS/Services/NotificationService/Notification.Tests/AutoData/AutoNotificationDataAttribute.cs
@@ -0,0 +1,27 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Notifications.API.Models.Requests;
+
+namespace BLL.Tests.AutoData
+{
+    public class AutoNotificationDataAttribute : AutoDataAttribute
+    {
+        private const string testAddress = "test@gmail.com";
+        private const string testUserId = "1";
+
+        public AutoNotificationDataAttribute() : base(() =>
+        {
+            var fixture = new Fixture();
+
+            fixture.Customize<EmailNotificationRequest>(x => x.With(r => r.RecieverEmailAddress, testAddress));
+            fixture.Customize<ClientNotificationRequest>(x => x.With(r => r.UserId, testUserId));
+
+            fixture.Customize(new AutoMoqCustomization());
+
+            return fixture;
+        })
+
+        { }
+    }
+}
---
+++ PMCS/Services/NotificationService/Notification.Tests/UnitTests/NotificationTests.cs
@@ -0,0 +1,49 @@
+using AutoFixture.Xunit2;
+using AutoMapper;
+using BLL.Tests.AutoData;
+using Moq;
+using Notifications.API.Controllers;
+using Notifications.API.Models.Requests;
+using Notifications.BLL.Interfaces.Services;
+using Notifications.BLL.Models.DTOs;
+using Shouldly;
+
+namespace BLL.Tests.UnitTests
+{
+    public class NotificationTests
+    {
+        [Theory, AutoNotificationData]
+        public async Task NotifyByEmail_ValidRequest_SendsEmail(
+            EmailNotificationRequest request,
+            EmailNotification notification,
+            [Frozen] Mock<IEmailService> emailServiceMock,
+            [Frozen] Mock<IMapper> mapperMock,
+            [Greedy] NotificationController controller)
+        {
+            emailServiceMock.Setup(x => x.SendNotification(notification)).Returns(Task.CompletedTask).Verifiable();
+            mapperMock.Setup(m => m.Map<EmailNotification>(request)).Returns(notification);
+
+            var result = await controller.NotifyByEmail(request);
+
+            result.ShouldNotBeNull();
+            emailServiceMock.Verify(x => x.SendNotification(notification), Times.Once);
+        }
+
+        [Theory, AutoNotificationData]
+        public async Task NotifyClient_ValidRequest_SendsNotification(
+            ClientNotificationRequest request,
+            ClientNotification notification,
+            [Frozen] Mock<IClientService> clientServiceMock,
+            [Frozen] Mock<IMapper> mapperMock,
+            [Greedy] NotificationController controller)
+        {
+            clientServiceMock.Setup(x => x.SendNotification(notification)).Returns(Task.CompletedTask).Verifiable();
+            mapperMock.Setup(m => m.Map<ClientNotification>(request)).Returns(notification);
+
+            var result = await controller.NotifyClient(request, default);
+
+            result.ShouldNotBeNull();
+            clientServiceMock.Verify(x => x.SendNotification(notification), Times.Once);
+        }
+    }
+}
