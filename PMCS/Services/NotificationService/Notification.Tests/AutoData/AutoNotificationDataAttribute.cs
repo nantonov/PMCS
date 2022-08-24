6013b8b5b3b7885a625a87a6c048e3b413b6516b
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-24 16:33:16 +0300

    Move contants to a separate file

--- PMCS/Services/NotificationService/Notification.Tests/AutoData/AutoNotificationDataAttribute.cs
+++ PMCS/Services/NotificationService/Notification.Tests/AutoData/AutoNotificationDataAttribute.cs
@@ -2,20 +2,18 @@
 using AutoFixture.AutoMoq;
 using AutoFixture.Xunit2;
 using Notifications.API.Models.Requests;
+using static Notification.Tests.AutoData.TestDataConstants;
 
 namespace BLL.Tests.AutoData
 {
     public class AutoNotificationDataAttribute : AutoDataAttribute
     {
-        private const string testAddress = "test@gmail.com";
-        private const string testUserId = "1";
-
         public AutoNotificationDataAttribute() : base(() =>
         {
             var fixture = new Fixture();
 
-            fixture.Customize<EmailNotificationRequest>(x => x.With(r => r.RecieverEmailAddress, testAddress));
-            fixture.Customize<ClientNotificationRequest>(x => x.With(r => r.UserId, testUserId));
+            fixture.Customize<EmailNotificationRequest>(x => x.With(r => r.RecieverEmailAddress, TestAddress));
+            fixture.Customize<ClientNotificationRequest>(x => x.With(r => r.UserId, TestUserId));
 
             fixture.Customize(new AutoMoqCustomization());
 
