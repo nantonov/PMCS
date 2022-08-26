3de5c88083dadd7a1f3278a509b78ce034bf8fe5
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-08-26 09:58:22 +0300

    Implement unit tests for NotificationService (#43)

---
+++ PMCS/Services/NotificationService/Notification.Tests/AutoData/AutoNotificationDataAttribute.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using Notifications.API.Models.Requests;
+using static Notification.Tests.AutoData.TestDataConstants;
+
+namespace BLL.Tests.AutoData
+{
+    public class AutoNotificationDataAttribute : AutoDataAttribute
+    {
+        public AutoNotificationDataAttribute() : base(() =>
+        {
+            var fixture = new Fixture();
+
+            fixture.Customize<EmailNotificationRequest>(x => x.With(r => r.RecieverEmailAddress, TestAddress));
+            fixture.Customize<ClientNotificationRequest>(x => x.With(r => r.UserId, TestUserId));
+
+            fixture.Customize(new AutoMoqCustomization());
+
+            return fixture;
+        })
+
+        { }
+    }
+}
---
+++ PMCS/Services/NotificationService/Notification.Tests/AutoData/TestDataConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace Notification.Tests.AutoData
+{
+    public static class TestDataConstants
+    {
+        public const string TestAddress = "test@gmail.com";
+        public const string TestUserId = "1";
+    }
+}
