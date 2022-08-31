16fc39f9e5ac53960f0417bd3ee82a31e02e135e
Daniil Feoktistov <whatdoessence@gmail.com>
2022-08-31 11:47:07 +0300

    Add authorization scheme

--- PMCS/Services/NotificationService/NotificationService/Program.cs
+++ PMCS/Services/NotificationService/NotificationService/Program.cs
@@ -1,14 +1,16 @@
 using FluentValidation.AspNetCore;
-using Notifications.BLL.DI;
-using System.Reflection;
-using Notifications.API.Middlewares;
-using Notifications.BLL.DI;
-using Serilog;
-using Serilog.Events;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Notifications.API.Middlewares;
 using Notifications.BLL.DI;
 using Notifications.BLL.Resources.Constants;
 using Notifications.BLL.SignalR.Hubs;
+using Serilog;
+using Serilog.Events;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,9 +26,62 @@ Log.Logger = Log.Logger = new LoggerConfiguration()
     .WriteTo.Console(outputTemplate: "{Timestamp:HH:mm} [{Level}] ({ThreadId}) ({ThreadName}) {Message}{NewLine}{Exception}")
     .CreateLogger();
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(config =>
+{
+    config.AddPolicy("DefaultPolicy",
+        builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = "https://localhost:5001/";
+    options.RequireHttpsMetadata = false;
+    options.Audience = "NotificationsAPI";
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = false,
+    };
+});
 builder.Services.AddAuthorization();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            Password = new OpenApiOAuthFlow
+            {
+                TokenUrl = new Uri("https://localhost:5001/connect/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    {"NotificationsAPI", "Notifications WebAPI"}
+                }
+            }
+        }
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "oauth2"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly())); ;
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
@@ -41,14 +96,27 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseCors("DefaultPolicy");
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.OAuthClientId("swagger-client-id");
+        options.OAuthScopeSeparator(" ");
+        options.OAuthClientSecret("client_secret");
+        options.DocExpansion(DocExpansion.List);
+    });
 }
 
 app.UseHttpsRedirection();
+
 app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
