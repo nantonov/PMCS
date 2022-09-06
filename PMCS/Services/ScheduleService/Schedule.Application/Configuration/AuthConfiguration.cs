2fcf85db34fcfdc5990775f370bd67a7448ce800
Daniil Feoktistov <whatdoessence@gmail.com>
2022-09-07 02:08:18 +0300

    Add auth service

--- PMCS/Services/ScheduleService/Schedule.Application/Configuration/AuthConfiguration.cs
+++ PMCS/Services/ScheduleService/Schedule.Application/Configuration/AuthConfiguration.cs
@@ -6,5 +6,11 @@
         public const string Authority = "https://localhost:5001/";
         public const bool RequireHttpsMetadata = false;
         public const bool ValidateAudience = false;
+
+        public const string ClientId = "client_id";
+        public const string ClientSecret = "pmcs-client-secret";
+
+        public const string PetScope = "PetsAPI";
+        public const string NotificationScope = "NotificationsAPI";
     }
 }
