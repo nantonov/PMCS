6b46484f8b003a0576baf521546d2b0bd05c5ab7
Daniil Feoktistov <whatdoessence@gmail.com>
2022-09-12 17:41:08 +0300

    Implement integration tests

--- PMCS/Services/ScheduleService/ScheduleService.InfrastructureTests/ReminderRepositoryIntegrationTests.cs
+++ PMCS/Services/ScheduleService/ScheduleService.InfrastructureTests/ReminderRepositoryIntegrationTests.cs
@@ -2,10 +2,10 @@ using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using Schedule.Domain.Entities;
-using Schedule.Domain.Enums;
 using Schedule.Domain.Repositories;
 using Schedule.Infrastructure;
 using Schedule.Infrastructure.Repositories;
+using ScheduleService.InfrastructureTests.TestData;
 
 namespace ScheduleService.InfrastructureTests
 {
@@ -22,14 +22,101 @@ namespace ScheduleService.InfrastructureTests
         }
 
         [Fact]
-        public async Task Insert_ValidData_InsertsReminder()
+        public async Task GetAll_RemindersDoNotExist_ReturnsEmptyList()
         {
-            var reminder = new Reminder(DateTime.UtcNow.AddDays(7), 1, 1, "Hello", NotificationType.Email,
-                ActionToRemindType.GoForWalk);
+            var result = await _repository.GetAll(default);
 
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task Get_NoReminderAreTriggered_ReturnsEmptyListOfTriggeredReminders()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+
+            var result = await _repository.Get(x => x.IsTriggered, default);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public async Task GetAll_RemindersExist_ReturnsReminders()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+            var result = await _repository.GetAll(default);
+
+            var actualCount = result.Count();
+
+            Assert.NotNull(result);
+            Assert.Equal(TestReminders.ValidReminders.Count, actualCount);
+        }
+
+        [Fact]
+        public async Task GetById_ReminderExists_ReturnsReminder()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+            var result = await _repository.GetById(1, default);
+
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task GetUserReminders_RemindersExist_ReturnsUserReminders()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+            var result = await _repository.GetUserReminders(TestReminders.TestUserId, default);
+
+            var actualCount = result.Count();
+
+            Assert.NotNull(result);
+            Assert.Equal(TestReminders.UserRemindersCount, actualCount);
+        }
+
+        [Theory]
+        [ClassData(typeof(ValidRemindersTestData))]
+        public async Task Insert_ValidData_InsertsReminder(Reminder reminder)
+        {
             var entity = await _repository.Insert(reminder, default);
 
             Assert.NotNull(entity);
+        }
+
+        [Fact]
+        public async Task Delete_ValidData_DeletesReminder()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+            var reminderToDelete = TestReminders.ValidReminders.FirstOrDefault();
+
+            var result = await _repository.Delete(reminderToDelete, default);
+
+            Assert.Equal(reminderToDelete.TriggerDateTime, result.TriggerDateTime);
+        }
+
+        [Fact]
+        public async Task Update_ReminderExists_UpdatesReminder()
+        {
+            await InitializeDatabaseWithValidRemindersAsync();
+            var reminderToUpdate = TestReminders.ValidReminders.FirstOrDefault();
+
+            reminderToUpdate.Update(TestReminders.UpdatedReminder.TriggerDateTime, TestReminders.UpdatedReminder.NotificationMessage,
+                TestReminders.UpdatedReminder.NotificationType, TestReminders.UpdatedReminder.ActionToRemindType);
+
+            var result = await _repository.Update(reminderToUpdate, default);
+
+            Assert.Equal(result.TriggerDateTime, TestReminders.UpdatedReminder.TriggerDateTime);
+            Assert.Equal(result.NotificationType, TestReminders.UpdatedReminder.NotificationType);
+            Assert.Equal(result.ActionToRemindType, TestReminders.UpdatedReminder.ActionToRemindType);
+            Assert.Equal(result.NotificationMessage, TestReminders.UpdatedReminder.NotificationMessage);
+        }
+
+        private async Task<List<Reminder>> InitializeDatabaseWithValidRemindersAsync()
+        {
+            foreach (var reminder in TestReminders.ValidReminders)
+            {
+                await _repository.Insert(reminder, default);
+            }
+
+            return TestReminders.ValidReminders;
         }
 
         public void Dispose()
---
+++ PMCS/Services/ScheduleService/ScheduleService.InfrastructureTests/TestData/ValidRemindersTestData.cs
@@ -0,0 +1,47 @@
+﻿using Schedule.Domain.Entities;
+using Schedule.Domain.Enums;
+using System.Collections;
+
+namespace ScheduleService.InfrastructureTests.TestData
+{
+    public class ValidRemindersTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new Reminder(DateTime.UtcNow.AddYears(1), 1, 1, "Jackie has to visit vet.", NotificationType.Email,
+                    ActionToRemindType.MakeVaccine)
+            };
+            yield return new object[]
+            {
+                new Reminder(DateTime.UtcNow.AddDays(1), 10, 1, "TestDescription", NotificationType.PersonalAccount,
+                    ActionToRemindType.GoForWalk)
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public static class TestReminders
+    {
+        public static List<Reminder> ValidReminders = new List<Reminder>()
+        {
+            new Reminder(DateTime.UtcNow.AddYears(1), 1, 1, "Jackie has to visit vet.", NotificationType.Email,
+                ActionToRemindType.MakeVaccine),
+            new Reminder(DateTime.UtcNow.AddDays(1), 10, 1, "TestDescription", NotificationType.PersonalAccount,
+                ActionToRemindType.GoForWalk),
+            new Reminder(DateTime.UtcNow.AddDays(1), 10, 2, "Feed Frank with meat", NotificationType.PersonalAccount,
+            ActionToRemindType.FeedPet)
+        };
+
+        public static Reminder UpdatedReminder = new Reminder((DateTime.UtcNow.AddDays(7)), 1, 1,
+            "Jackie has to have a meal.", NotificationType.PersonalAccount, ActionToRemindType.FeedPet);
+
+        public const int TestUserId = 1;
+        public static int UserRemindersCount => ValidReminders.Count(x => x.UserId == TestUserId);
+    }
+}
