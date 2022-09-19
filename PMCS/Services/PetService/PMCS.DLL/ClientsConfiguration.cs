fe7276cef4c7534806365801dc376b6628ee5973
Daniil Feoktistov <whatdoessence@gmail.com>
2022-09-19 20:37:51 +0300

    Remove reminders related to deleted pet

---
+++ PMCS/Services/PetService/PMCS.DLL/ClientsConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace PMCS.BLL
+{
+    public static class ClientsConfiguration
+    {
+        public const string ScheduleClientName = "Schedule";
+        public const string ScheduleServiceAddress = "https://localhost:7064";
+        public const int ScheduleClientRetryCount = 3;
+        public const int ScheduleSleepDurationInMilliseconds = 100;
+    }
+}
--- PMCS/Services/PetService/PMCS.DLL/Services/PetService.cs
+++ PMCS/Services/PetService/PMCS.DLL/Services/PetService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using PMCS.BLL;
 using PMCS.DAL.Entities;
 using PMCS.DAL.Interfaces.Repositories;
 using PMCS.DLL.Exceptions;
@@ -10,10 +11,15 @@ namespace PMCS.DLL.Services
     public class PetService : GenericService<PetModel, PetEntity>, IPetService
     {
         private readonly IOwnerService _ownerService;
+        private readonly IHttpClientFactory _httpClientFactory;
 
-        public PetService(IPetRepository repository, IMapper mapper, IOwnerService ownerService) : base(repository, mapper)
+        public PetService(IPetRepository repository,
+            IMapper mapper,
+            IOwnerService ownerService,
+            IHttpClientFactory httpClientFactory) : base(repository, mapper)
         {
             _ownerService = ownerService;
+            _httpClientFactory = httpClientFactory;
         }
 
         public override async Task<PetModel> Update(PetModel model, CancellationToken cancellationToken)
@@ -24,6 +30,22 @@ namespace PMCS.DLL.Services
             var entity = _mapper.Map<PetEntity>(model);
 
             var result = await _repository.Update(entity, cancellationToken);
+
+            return _mapper.Map<PetModel>(result);
+        }
+
+        public override async Task<PetModel> Delete(int id, CancellationToken cancellationToken)
+        {
+            if (!await IsModelExists(id, cancellationToken)) throw new ModelIsNotFoundException();
+
+            var result = await _repository.Delete(id, cancellationToken);
+
+            if (result != null)
+            {
+                var client = _httpClientFactory.CreateClient(ClientsConfiguration.ScheduleClientName);
+
+                await client.DeleteAsync($"/api/Reminder/api/Pet/{id}");
+            }
 
             return _mapper.Map<PetModel>(result);
         }
