e9b5d51cdb05f26d333a8fd2a38f64d3dd5fb479
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-08 17:53:55 +0300

    Move reducers to same folder

--- PMCS/Client/react-pmcs/src/Components/Content/Pets/PetsContainer.jsx
+++ PMCS/Client/react-pmcs/src/Components/Content/Pets/PetsContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import Pets from './Pets';
-import { deletePet, editPet, setPets, addPet} from '../../../redux/reducers/petsReducer/petsReducer';
+import { deletePet, editPet, setPets, addPet} from '../../../redux/reducers/petsReducer';
 
 function mapStateToProps(state) {
     return { pets: state.petsPage.pets }
--- PMCS/Client/react-pmcs/src/Components/Header/Profile/ProfileContainer.jsx
+++ PMCS/Client/react-pmcs/src/Components/Header/Profile/ProfileContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import Profile from './Profile';
-import {editOwner, setOwner, addOwner } from '../../../redux/reducers/ownersReducer/ownersReducer';
+import {editOwner, setOwner, addOwner } from '../../../redux/reducers/ownersReducer';
 
 function mapStateToProps(state) {
     return { owner: state.profile.owner }
