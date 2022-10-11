e10a61cad43627ff5078fdff280bd36a95514637
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-12 00:33:03 +0300

    Apply thunk middleware

--- PMCS/Client/react-pmcs/src/redux/redux-store.js
+++ PMCS/Client/react-pmcs/src/redux/redux-store.js
@@ -1,13 +1,14 @@
-import { combineReducers, legacy_createStore as createStore } from 'redux';
+import { combineReducers, applyMiddleware, legacy_createStore as createStore } from 'redux';
 import ownersReducer from './reducers/ownersReducer';
-import petsReducer from "./reducers/petsReducer";
+import petsReducer from "./Pets/petsReducer";
+import thunkMiddleware from 'redux-thunk';
 
 let reducersBatch = combineReducers({
     petsPage: petsReducer,
     profile: ownersReducer
 });
 
-let store = createStore(reducersBatch);
+let store = createStore(reducersBatch, applyMiddleware(thunkMiddleware));
 
 
 export default store;
