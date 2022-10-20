b7dc63277b72706676018eb708425bb4a02f62ab
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-20 23:10:36 +0300

    Move config constants to static class

---
+++ PMCS/Services/NotificationService/Notifications.BLL/SignalR/Configuration/NotificationHubConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace Notifications.BLL.SignalR.Configuration
+{
+    public static class NotificationHubConfiguration
+    {
+        public const string HandlingMethodURL = "RecieveNotification";
+
+        public const string HubURL = "/notificationHub";
+    }
+}
