60f8765c9ddb255e1a16d6078d671d701a5e6ce2
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-21 15:47:55 +0300

    Put sxProps to separate file

--- PMCS/Client/react-pmcs/src/Components/ProgressBars/CircularProgressWithLabel.jsx
+++ PMCS/Client/react-pmcs/src/Components/ProgressBars/CircularProgressWithLabel.jsx
@@ -2,23 +2,13 @@ import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { circularBarSxProps } from './sxProps';
 
 const CircularProgressWithLabel = (props) => {
     return (
-        <Box sx={{ position: 'relative', display: 'inline-flex'}}>
+        <Box sx={{ position: 'relative', display: 'inline-flex' }}>
             <CircularProgress variant="determinate" {...props} />
-            <Box
-                sx={{
-                    top: 0,
-                    left: 0,
-                    bottom: 0,
-                    right: 0,
-                    position: 'absolute',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
+            <Box sx={circularBarSxProps}>
                 <Typography variant="subtitle1" component="div" color="text.secondary" >
                     {`${props.value}%`}
                 </Typography>
