8447c114880e0ed3ac5d48f09453c00d0e319fb5
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-25 16:53:05 +0300

    Remove useless lines

--- PMCS/Services/AuthorizationService/IdentityServer/Account/ExternalController.cs
+++ PMCS/Services/AuthorizationService/IdentityServer/Account/ExternalController.cs
@@ -1,9 +1,7 @@
 using IdentityModel;
 using IdentityServer.Models;
 using IdentityServer4;
-using IdentityServer4.Events;
 using IdentityServer4.Services;
-using IdentityServer4.Stores;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -18,23 +16,13 @@ namespace IdentityServerHost.Quickstart.UI
     {
         private readonly UserManager<User> _users;
         private readonly IIdentityServerInteractionService _interaction;
-        private readonly IClientStore _clientStore;
-        private readonly ILogger<ExternalController> _logger;
-        private readonly IEventService _events;
-
         public ExternalController(
             IIdentityServerInteractionService interaction,
-            IClientStore clientStore,
-            IEventService events,
-            ILogger<ExternalController> logger,
             UserManager<User> users)
         {
             _users = users;
 
             _interaction = interaction;
-            _clientStore = clientStore;
-            _logger = logger;
-            _events = events;
         }
 
         [HttpGet]
@@ -65,56 +53,33 @@ namespace IdentityServerHost.Quickstart.UI
         public async Task<IActionResult> Callback()
         {
             var result = await HttpContext.AuthenticateAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
+
             if (result?.Succeeded != true)
             {
                 throw new Exception("External authentication error");
             }
 
-            if (_logger.IsEnabled(LogLevel.Debug))
-            {
-                var externalClaims = result.Principal.Claims.Select(c => $"{c.Type}: {c.Value}");
-                _logger.LogDebug("External claims: {@claims}", externalClaims);
-            }
-
-            var (user, provider, providerUserId, claims) = FindUserFromExternalProvider(result);
+            var (user, provider, claims) = FindUserFromExternalProvider(result);
 
             if (user == null)
             {
                 user = await AutoProvisionUser(claims);
             }
 
-            var additionalLocalClaims = new List<Claim>();
-            var localSignInProps = new AuthenticationProperties();
-            ProcessLoginCallback(result, additionalLocalClaims, localSignInProps);
-
-            var isuser = new IdentityServerUser(user.Id.ToString())
+            await HttpContext.SignInAsync(new IdentityServerUser(user.Id.ToString())
             {
                 DisplayName = user.UserName,
-                IdentityProvider = provider,
-                AdditionalClaims = additionalLocalClaims
-            };
-
-            await HttpContext.SignInAsync(isuser, localSignInProps);
+                IdentityProvider = provider
+            });
 
             await HttpContext.SignOutAsync(IdentityServerConstants.ExternalCookieAuthenticationScheme);
 
             var returnUrl = result.Properties.Items["returnUrl"] ?? "~/";
 
-            var context = await _interaction.GetAuthorizationContextAsync(returnUrl);
-            await _events.RaiseAsync(new UserLoginSuccessEvent(provider, providerUserId, user.Id.ToString(), user.UserName, true, context?.Client.ClientId));
-
-            if (context != null)
-            {
-                if (context.IsNativeClient())
-                {
-                    return this.LoadingPage("Redirect", returnUrl);
-                }
-            }
-
             return Redirect(returnUrl);
         }
 
-        private (User user, string provider, string providerUserId, IEnumerable<Claim> claims) FindUserFromExternalProvider(AuthenticateResult result)
+        private (User user, string provider, IEnumerable<Claim> claims) FindUserFromExternalProvider(AuthenticateResult result)
         {
             var externalUser = result.Principal;
 
@@ -130,7 +95,7 @@ namespace IdentityServerHost.Quickstart.UI
 
             var user = _users.FindByIdAsync(providerUserId).GetAwaiter().GetResult();
 
-            return (user, provider, providerUserId, claims);
+            return (user, provider, claims);
         }
 
         private async Task<User> AutoProvisionUser(IEnumerable<Claim> claims)
@@ -150,21 +115,6 @@ namespace IdentityServerHost.Quickstart.UI
             await _users.AddClaimsAsync(user, claims);
 
             return user;
-        }
-
-        private void ProcessLoginCallback(AuthenticateResult externalResult, List<Claim> localClaims, AuthenticationProperties localSignInProps)
-        {
-            var sid = externalResult.Principal.Claims.FirstOrDefault(x => x.Type == JwtClaimTypes.SessionId);
-            if (sid != null)
-            {
-                localClaims.Add(new Claim(JwtClaimTypes.SessionId, sid.Value));
-            }
-
-            var idToken = externalResult.Properties.GetTokenValue("id_token");
-            if (idToken != null)
-            {
-                localSignInProps.StoreTokens(new[] { new AuthenticationToken { Name = "id_token", Value = idToken } });
-            }
         }
     }
 }
