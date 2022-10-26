0899285b0f2fa3d9845feec5c8b0e46eb1805cc9
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-26 13:43:02 +0300

    Refactor pets redux

---
+++ PMCS/Client/react-pmcs/src/redux/Pets/petsReducer.ts
@@ -0,0 +1,30 @@
+import { IPet } from "../../common/models/IPet";
+import { SET_IS_FETCHING, SET_PETS } from "./constants";
+import * as actions from './actions';
+
+export type PetsState = {
+    pets: Array<IPet>,
+    isFetching: boolean
+};
+
+let initialState: PetsState = {
+    pets: [],
+    isFetching: false
+};
+
+export type PetsActions = ReturnType<typeof actions[keyof typeof actions]>;
+
+const petsReducer = (state = initialState, action: PetsActions) => {
+    switch (action.type) {
+        case SET_PETS: {
+            return { ...state, pets: action.payload }
+        }
+        case SET_IS_FETCHING: {
+            return { ...state, isFetching: action.payload }
+        }
+        default:
+            return state;
+    }
+}
+
+export default petsReducer;
