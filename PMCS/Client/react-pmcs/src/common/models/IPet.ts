5296bc6cd654195ee7e34134546c835097620220
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-26 14:36:46 +0300

    Declare missed fields in interfaces

--- PMCS/Client/react-pmcs/src/common/models/IPet.ts
+++ PMCS/Client/react-pmcs/src/common/models/IPet.ts
@@ -1,3 +1,17 @@
+import { IOwner } from "./IOwner";
+import { IVaccine } from "./IVaccine";
+import { IWalking } from "./IWalking";
+import { IMeal } from "./IMeal";
+
 export interface IPet {
     id: number;
+    name: string;
+    info: string;
+    birthDate: string;
+    weight: number;
+
+    owner: IOwner;
+    meals: Array<IMeal>;
+    walkings: Array<IWalking>;
+    vaccines: Array<IVaccine>;
 }
---
+++ PMCS/Client/react-pmcs/src/common/models/IVaccine.ts
@@ -0,0 +1,6 @@
+export interface IVaccine {
+    id: number;
+    title: string;
+    desctiption: string;
+    dateTime: string;
+}
