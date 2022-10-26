f40f23b9378ae295e995198006fb6dc68e2bb135
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-26 12:47:46 +0300

    Typify notifications reducer, state, selectors, actions

--- PMCS/Client/react-pmcs/src/redux/Notifications/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectMessages = (state) => state.notifications.messages;
-
-export const getMessages = createSelector(selectMessages, (messages) => {
-    return messages.filter((value, index, self) => self.indexOf(value) === index);
-});
