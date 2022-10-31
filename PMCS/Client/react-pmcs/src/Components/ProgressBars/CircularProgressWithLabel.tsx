bde83ac3dccf8260c6007a824fcc933bc963ef7e
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-31 21:58:45 +0300

    Declare types for progress bar

--- PMCS/Client/react-pmcs/src/Components/ProgressBars/CircularProgressWithLabel.tsx
+++ PMCS/Client/react-pmcs/src/Components/ProgressBars/CircularProgressWithLabel.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress, { CircularProgressProps } from '@mui/material/CircularProgress';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { circularBarSxProps } from './sxProps';
+import { SxProps } from '@mui/material';
 
-const CircularProgressWithLabel = (props) => {
+type Props = { value: number; } & CircularProgressProps;
+
+const CircularProgressWithLabel: React.FC<Props> = (props) => {
     return (
         <Box sx={{ position: 'relative', display: 'inline-flex' }}>
             <CircularProgress variant="determinate" {...props} />
