e0c1872754bf54654cc4cc166c2889cc5378c112
Daniil Feoktistov <whatdoessence@gmail.com>
2022-10-31 15:42:35 +0300

    Refactor configs

--- PMCS/Client/react-pmcs/src/configuration/authConfig.ts
+++ PMCS/Client/react-pmcs/src/configuration/authConfig.ts
@@ -1,12 +1,15 @@
-const authConfig = {
-    authority:'https://localhost:5001',
-    client_id:'react-client-id',
-    scope:'openid profile email ScheduleAPI PetAPI',
-    response_type:'code',
+import { UserManagerSettings} from "oidc-client";
 
-    redirect_uri:'http://localhost:3000/callback',
-    post_logout_redirect_uri:'http://localhost:3000/logout',
+const authConfig: UserManagerSettings = {
+    authority: 'https://localhost:5001',
+    client_id: 'react-client-id',
+    scope: 'openid profile email ScheduleAPI PetAPI',
+    response_type: 'code',
+
+    redirect_uri: 'http://localhost:3000/callback',
+    post_logout_redirect_uri: 'http://localhost:3000/logout',
     silent_redirect_uri: `http://localhost:3000/refresh`,
+    automaticSilentRenew: true,
 
     loadUserInfo: true,
     response_mode: 'query',
--- PMCS/Client/react-pmcs/src/configuration/axiosConfig.js
+++
@@ -1,5 +0,0 @@
-export const axiosConfig = {
-    baseURL: 'https://localhost:7045',
-    AccessControlAllowOrigin: '*',
-    AccessControlAllowMethods: 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-};
