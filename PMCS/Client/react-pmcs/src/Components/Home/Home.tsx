31a2e976a2a292c284de1fa283874c525379aaf4
Daniil Feoktistov <75939461+moltenessence@users.noreply.github.com>
2022-11-01 19:53:58 +0300

    Refactor configs (#94)

--- PMCS/Client/react-pmcs/src/Components/Home/Home.tsx
+++ PMCS/Client/react-pmcs/src/Components/Home/Home.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import s from './Home.module.css';
 import { useAuthContext } from '../Auth/AuthProvider';
 
-const Home = props => {
+type Props = {};
+
+const Home: React.FC<Props> = (props = {}) => {
     const { isAuth } = useAuthContext();
 
     return (
         <section className={s.wrapper}>
             <h1>Home</h1>
-            {isAuth &&  <div className={s.text}>
+            {isAuth && <div className={s.text}>
                 Hello, welcome to Pets Monitoring and Control System main page!
             </div>}
-           {!isAuth && <div className={s.text}>You are not authenticated. Please, login.</div>}
+            {!isAuth && <div className={s.text}>You are not authenticated. Please, login.</div>}
         </section>
     );
 }
