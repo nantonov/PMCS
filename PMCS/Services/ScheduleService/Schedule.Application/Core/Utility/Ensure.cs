249e1ffe6e6701f75590c2cef45f349c8ae7b41c
Daniil Feoktistov <whatdoessence@gmail.com>
2022-11-09 10:47:28 +0300

    Fix code smell in utility

--- PMCS/Services/ScheduleService/Schedule.Application/Core/Utility/Ensure.cs
+++ PMCS/Services/ScheduleService/Schedule.Application/Core/Utility/Ensure.cs
@@ -4,8 +4,10 @@ namespace Schedule.Application.Core.Utility
 {
     public static class Ensure
     {
-        public static (bool succeed, string? Address) IsValidEmail(string? email)
+        public static (bool succeed, string? address) IsValidEmail(string? email)
         {
+            if (email is null) return (false, null);
+
             var succeed = MailAddress.TryCreate(email, out var validEmail);
 
             return (succeed, validEmail?.Address);
